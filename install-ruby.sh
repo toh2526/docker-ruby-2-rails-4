9682145aa01d5fce2a3222b630090bd45e23b277
Ittipan Langkulanon <toh2526@msn.com>
2014-03-31 18:33:06 +0700

    - add more packages required by gems - add more gems required by gems

--- install-ruby.sh
+++ install-ruby.sh
@@ -5,6 +5,7 @@
 # We are using fixed version number because above method does not work on script called by Dockerfile.
 LATEST_RUBY_VER=2.1.1
 
+sudo apt-get install libxslt-dev libxml2-dev
 wget http://cache.ruby-lang.org/pub/ruby/stable/ruby-$LATEST_RUBY_VER.tar.gz
 tar -xzf ruby-$LATEST_RUBY_VER.tar.gz
 rm ruby-$LATEST_RUBY_VER.tar.gz
@@ -19,3 +20,4 @@ echo 'gem: --no-document' > /etc/gemrc
 echo 'gem: --no-document' > ~/.gemrc
 gem install bundler
 gem install rails
+gem install nokogiri
